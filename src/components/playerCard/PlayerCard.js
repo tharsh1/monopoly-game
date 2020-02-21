37e5be904194d0a3b5d3b4ddc996b0f70a0504e4
Asgar Ali <asgaralipq@gmail.com>
2020-02-21 23:40:50 +0530

    player card UI

--- src/components/playerCard/PlayerCard.js
+++ src/components/playerCard/PlayerCard.js
@@ -9,7 +9,60 @@ class PlayerCard extends React.Component{
     render(){
         return (
             <div className='player1-container' style={{border: `${this.props.currentPlayer ? '2px solid green': '1px solid gray'}`}}>
-
+                <div className='player1-bar'>
+                    <div className='player1-name'>
+                        {this.playerName}
+                    </div>
+                    
+                    <div className='player1-balance'>
+                        {this.balance}
+                    </div>
+                </div>
+                <div className='FixedHeightContainer'>
+                
+                    <div className='player1-property'>
+                    <ul class="list-group">
+                        <div id='property1'>
+                        <li>Mumbai
+                        <span class="badge">100000</span>
+                        </li>
+                        </div>
+                        
+                        <div id='property2'>
+                        <li>Pune
+                        <span class="badge">40000</span>
+                        </li>
+                        </div>
+                        
+                        <div id='property3'>
+                        <li>Nashik
+                        <span class="badge">25000</span>
+                        </li>
+                        </div>
+                        
+                        <div id='property4'>
+                        <li>Navi Mumbai
+                        <span class="badge">33000</span>
+                        </li>
+                        </div>
+                        
+                        <div id='property5'>
+                        <li>Bangalore
+                        <span class="badge">44000</span>
+                        </li>
+                        </div>
+                        
+                        <div id='property6'>
+                        <li>Kolkata
+                        <span class="badge">Free</span>
+                        </li>
+                        </div>
+                        
+                    </ul> 
+                       
+                    </div>
+                        
+                </div>
             </div>
         )
     }
