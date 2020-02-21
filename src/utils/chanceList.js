a68bb892f92fe2ad9ebecc239df149f2f316f07a
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-02-21 22:40:20 +0530

    chance cards list

---
+++ src/utils/chanceList.js
@@ -0,0 +1,59 @@
+const chanceList = [
+    {
+        description:'Advance to "Go"',
+        goToPosition:0,
+        move:true,
+        buttonTag:'GO'
+    },
+    {
+        description:'Go to Jail',
+        goToPosition:10,
+        move:true,
+        buttonTag:'GO'
+    },
+    {
+        description:'Advance to Paul Mall',
+        goToPosition:11,
+        move:true,
+        buttonTag:'GO'
+    },
+    {
+        description:'Take a trip to MarylBorne Station',
+        goToPosition:15,
+        move:true,
+        buttonTag:'GO'
+    },
+    {
+        description:'Advance to Trafalgar Square',
+        goToPosition:24,
+        move:true,
+        buttonTag:'GO'
+    },
+    {
+        description:'Advance to MayFair',
+        goToPosition:39,
+        move:true,
+        buttonTag:'GO'
+    },
+    {
+        description:'Pay School Fees of 300 pounds',
+        pay:300,
+        buttonTag:'PAY'
+    },
+    {
+        description:'your building loans Mature receive 300 pounds',
+        get:300,
+        buttonTag:'COLLECT'
+    },
+    {
+        description:'You have won a crossword competition collect 200 pounds',
+        get:200,
+        buttonTag:'COLLECT'
+    },
+    {
+        description:'bank pays you dividend of 1000 pounds',
+        get:1000,
+        buttonTag:'COLLECT'
+    }
+]
+export default chanceList;
