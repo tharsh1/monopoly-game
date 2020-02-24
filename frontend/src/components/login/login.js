829ba08f825de9fcb3bcf0a9ab6e64e4efad90ca
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-02-24 21:40:27 +0530

    added react router

---
+++ frontend/src/components/login/login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './login.css';
+
+class Login extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    
+
+    render(){
+        return (
+            <div>
+                login
+            </div>
+        )
+    }
+}
+
+export default Login;
