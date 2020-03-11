0259da8eb0a3e0a1ea0cb361dce87b6072f853b2
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-03-11 23:14:53 +0530

    Updates

---
+++ precache-manifest.983e85c52deab76a4c6d486503b462e5.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9840abf42fc8c7e6adfb893d1d2685ae",
+    "url": "/monopoly-game/index.html"
+  },
+  {
+    "revision": "d73157e1d0727533defc",
+    "url": "/monopoly-game/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "f83cc2bced80ab8a60e7",
+    "url": "/monopoly-game/static/css/main.ec9780e6.chunk.css"
+  },
+  {
+    "revision": "d73157e1d0727533defc",
+    "url": "/monopoly-game/static/js/2.f2a640bf.chunk.js"
+  },
+  {
+    "revision": "46f63f8626516ab37e48a023f0c6fcd4",
+    "url": "/monopoly-game/static/js/2.f2a640bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f83cc2bced80ab8a60e7",
+    "url": "/monopoly-game/static/js/main.85244f77.chunk.js"
+  },
+  {
+    "revision": "654aa25dad5878b44b90",
+    "url": "/monopoly-game/static/js/runtime-main.b2e67d60.js"
+  },
+  {
+    "revision": "938a079d60e0d53db86e2ebc330a80e7",
+    "url": "/monopoly-game/static/media/blue-pawn.938a079d.svg"
+  },
+  {
+    "revision": "9976f91adb62e217572bbe6626e2834d",
+    "url": "/monopoly-game/static/media/buyicon.9976f91a.svg"
+  },
+  {
+    "revision": "c907543ceec055cbf26e116cc00bf448",
+    "url": "/monopoly-game/static/media/cancel.c907543c.svg"
+  },
+  {
+    "revision": "38ba986b817103ef4640d351bac0ed6a",
+    "url": "/monopoly-game/static/media/dice1.38ba986b.svg"
+  },
+  {
+    "revision": "00410b665b80a3919d88bf1649b0f08a",
+    "url": "/monopoly-game/static/media/dice2.00410b66.svg"
+  },
+  {
+    "revision": "cf305e27b35a225ae3f4be54f613c677",
+    "url": "/monopoly-game/static/media/dice3.cf305e27.svg"
+  },
+  {
+    "revision": "3e176afc9122db45192f6b64e734049c",
+    "url": "/monopoly-game/static/media/dice4.3e176afc.svg"
+  },
+  {
+    "revision": "2180d6ccb223a824a26c1e7a5c1b5039",
+    "url": "/monopoly-game/static/media/dice5.2180d6cc.svg"
+  },
+  {
+    "revision": "331962690226b117338bfcf9607bf341",
+    "url": "/monopoly-game/static/media/dice6.33196269.svg"
+  },
+  {
+    "revision": "6431d99117846f7570d280b59f025594",
+    "url": "/monopoly-game/static/media/green-pawn.6431d991.svg"
+  },
+  {
+    "revision": "e586ec1e492200d26ae3f828e03f3733",
+    "url": "/monopoly-game/static/media/monopoly-board.e586ec1e.jpg"
+  },
+  {
+    "revision": "ac22cd1d15b0d1d14f20885a86d14e66",
+    "url": "/monopoly-game/static/media/red-pawn.ac22cd1d.svg"
+  },
+  {
+    "revision": "79c0497f1e0caf0133b3d40c65d52f57",
+    "url": "/monopoly-game/static/media/yellow-pawn.79c0497f.svg"
+  }
+]);
