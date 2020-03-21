70037abe8f520e4e51ebaf114a526668dc5f29d4
Deepanshu Vangani <vanganideepanshu@gmail.com>
2020-03-21 19:40:26 +0530

    deployment changes

--- backend/index.js
+++ backend/index.js
@@ -1,58 +1,65 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var cors = require('cors')
-var path = require('path');
-var config = require('./config');
-const mongoose = require('mongoose');
-const game = require('./model');
-var bodyParser = require('body-parser');
-const shortid = require('shortid');
+var cors = require("cors");
+var path = require("path");
+var config = require("./config");
+const mongoose = require("mongoose");
+const game = require("./model");
+var bodyParser = require("body-parser");
+const shortid = require("shortid");
 
+app.use(bodyParser());
 
-app.use(bodyParser())
+mongoose.connect(
+  config.db_url,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  function(err, db) {
+    if (err) throw err;
+    console.log("Connected to database Successfully!");
+  }
+);
 
-mongoose.connect(config.db_url,{useNewUrlParser:true,useUnifiedTopology:true},
-    function(err, db) {
-        if (err) throw(err);
-        console.log("Connected to database Successfully!");
-      });
+app.use(cors());
 
-app.use(cors())
-
-app.post('/startNewGame', async (req,res)=>{
-    console.log(req.body)
-    var save = await game.create({gameId : shortid.generate(),state:req.body.state , winner:req.body.winner});
-    res.send(save)
+app.post("/startNewGame", async (req, res) => {
+  console.log(req.body);
+  var save = await game.create({
+    gameId: shortid.generate(),
+    state: req.body.state,
+    winner: req.body.winner
+  });
+  res.send(save);
 });
 
-app.post('/saveGame' , async (req,res)=>{
-    try{
-        var set = await game.findOneAndUpdate(
-            {
-                gameId: req.body.gameId
-            },
-            {
-                $set: {
-                    state: req.body.state,
-                    winner: req.body.winner
-                }
-            }
-        );
-        res.send({code:1 , message: "game saved"});
-    }catch(error){
-        res.send({code:0 , message: "something went wrong"});
-    }
+app.post("/saveGame", async (req, res) => {
+  try {
+    var set = await game.findOneAndUpdate(
+      {
+        gameId: req.body.gameId
+      },
+      {
+        $set: {
+          state: req.body.state,
+          winner: req.body.winner
+        }
+      }
+    );
+    res.send({ code: 1, message: "game saved" });
+  } catch (error) {
+    res.send({ code: 0, message: "something went wrong" });
+  }
 });
 
-app.post('/getGame' , async (req,res)=>{
-    const currGame = await game.findOne({gameId: req.body.gameId});
-    console.log(currGame);
-    if(currGame != null){
-        res.send({code:1 , data: currGame})
-    }
-    else{
-        res.send({code:0 , message: "no such game exists"})
-    }
+app.post("/getGame", async (req, res) => {
+  const currGame = await game.findOne({ gameId: req.body.gameId });
+  console.log(currGame);
+  if (currGame != null) {
+    res.send({ code: 1, data: currGame });
+  } else {
+    res.send({ code: 0, message: "no such game exists" });
+  }
 });
 
-app.listen(3000);
+app.listen(process.env.port || 3000, () => {
+  console.log("monopoly backend started");
+});
